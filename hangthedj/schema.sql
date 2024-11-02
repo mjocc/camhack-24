4b8aa827695e6b36c47c141cb92caa502848767b
mjocc <61629073+mjocc@users.noreply.github.com>
2024-11-02 15:54:24 +0000

    Add test data to db setup file

--- hangthedj/schema.sql
+++ hangthedj/schema.sql
@@ -4,3 +4,7 @@ CREATE TABLE IF NOT EXISTS result (
     name_2 TEXT,
     match_score INTEGER
 );
+
+INSERT INTO result VALUES ("118230ca-e0e8-43c5-9f5e-4a787f3d9405", "Becky", "Ben", 57);
+INSERT INTO result VALUES ("62ad919d-df86-468e-8fcc-0b1b33a48d6f", "Hubert", "Alastair", 93);
+INSERT INTO result VALUES ("becbad47-90a6-442e-9e76-5edcc2c0568a", "John", "Joan", 17);
